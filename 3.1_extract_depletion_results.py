0b26d785bac1bed8401ee041c9806fca76de20f0
jlball <jlball813@gmail.com>
2022-06-14 16:01:04 -0400

    depletion analysis added

---
+++ 3.1_extract_depletion_results.py
@@ -0,0 +1,18 @@
+import openmc.deplete
+import matplotlib.pyplot as plt
+
+results = openmc.deplete.ResultsList.from_hdf5("depletion_results.h5")
+
+times, Pu_atoms = results.get_atoms("4", 'Pu239')
+_, U_atoms = results.get_atoms("4", 'U238')
+
+fig, ax = plt.subplots()
+ax.plot(times, Pu_atoms)
+ax.plot(times, U_atoms)
+
+ax.set(xlabel='time (s)', ylabel='Number of atoms',
+       title='Build up of Pu-239')
+ax.grid()
+plt.savefig('atoms.png')
+plt.show()
+
